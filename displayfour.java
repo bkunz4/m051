eea0db9fb4afe250cec8259992ae299b56eff26d
bkunz4 <136372473+bkunz4@users.noreply.github.com>
2023-11-28 19:41:23 +0000

    Initial commit

---
+++ displayfour.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class displayfour extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Creates a pane to hold the images views
+    GridPane pane = new GridPane();
+
+    // Puts nodes in the pane
+    pane.add(new ImageView(new Image("image/uk.gif")), 0, 0);
+    pane.add(new ImageView(new Image("image/ca.gif")), 1, 0);
+    pane.add(new ImageView(new Image("image/china.gif")), 0, 1);
+    pane.add(new ImageView(new Image("image/us.gif")), 1, 1);
+
+    Scene scene = new Scene(pane);
+    primaryStage.setTitle("displayfour"); // Sets the stage title
+    primaryStage.setScene(scene); // Puts scene in the stage
+    primaryStage.show(); // Displays the stage.
+  }
+}
